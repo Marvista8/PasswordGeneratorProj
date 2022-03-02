105def8e24a55ec3f43d7798e210e47709d0f6ae
Margaret Farmer <86688818+Marvista8@users.noreply.github.com>
2022-03-01 19:24:43 -0500

    Here is the Css

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //Password Generator project
+var characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLength = 20;
+var password = "";
+
+
+for (let i =0; i < passwordLength.length; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+}
