da1b512d1d4fb521943a836268145d24a09a4cac
Margaret Farmer <86688818+Marvista8@users.noreply.github.com>
2022-03-05 19:28:24 -0500

    heads up everyone please look over code notes, had a problem setting variable due inporper (deprecated method which I changed) view comments, thank you.

--- script.js
+++ script.js
@@ -5,75 +5,79 @@ They be able to have a choice between uppercase, lowercase, numbers and symbols 
 There should be checkboxes that let them decide between the values stated.
 The user should be able to select their character length between the slider or enter a number in the number input.
 The main header should be able to transform and reveal the password.
+EXTRA NOTE event target was not working because was undefined evt pram changed to e and e.target then dot notation to value imput for slider and number input.
+Learned about toUppercase today should have used that, lesson learned.
 */
 
-//button for first page
+//button for first page leads to the generator.
 function nextPage() {
-    let nextPageBtn = document.getElementsByClassName("bgnBtn");
-    console.log(nextPageBtn);
-    nextPageBtn = onclick(location.href="password.html");
+    let nextBtn = document.getElementsByClassName("bgnBtn");
+    console.log(nextBtn);
+    nextBtn = onclick(location.href="password.html");
 }
 
 //These are all of the global elements that are seen and are called by the functions, this includes all the unicode values that were set up.
-const characterAmountRange = document.getElementById('characterAmountRange')
-const characterAmountNumber = document.getElementById('characterAmountNumber')
-const includeUppercaseElement = document.getElementById('includeUppercase')
-const includeNumbersElement = document.getElementById('includeNumbers')
-const includeSymbolsElement = document.getElementById('includeSymbols')
-const form = document.getElementById('passwordGeneratorForm')
+const charAmountRange = document.getElementById('characterAmountRange')
+const charAmountNumber = document.getElementById('characterAmountNumber')
+const includeUppercaseEl = document.getElementById('includeUppercase')
+const includeNumbersEl = document.getElementById('includeNumbers')
+const includeSymbolsEl = document.getElementById('includeSymbols')
+const pswdForm = document.getElementById('passwordGeneratorForm')
 const passwordDisplay = document.getElementById('passwordDisplay')
+
 //unicode letter, number, and symbol values for arrays (new trick did not know about .concat to combine items)
-const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
-const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
-const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
-  arrayFromLowToHigh(58, 64)
+const uppercaseChars = hiLowArray(65, 90)     //uppercase letters a #97
+const lowercaseChars = hiLowArray(97, 122)    //lowercase lettrs A #65
+const numberChars = hiLowArray(48, 57)    //numbers 0 - 9 #48 - 57
+const symbolChars = hiLowArray(33, 47).concat(        //Symbols #33 - 47 concatenate together to prepare for loop (4 arrays into 1)
+  hiLowArray(58, 64)
 ).concat(
-  arrayFromLowToHigh(91, 96)
+  hiLowArray(91, 96)
 ).concat(
-  arrayFromLowToHigh(123, 126)
+  hiLowArray(123, 126)
 )
+
 //eventlisteners for the inputs to be synced together
 
-characterAmountNumber.addEventListener('input', syncCharacterAmount)
-characterAmountRange.addEventListener('input', syncCharacterAmount)
+charAmountNumber.addEventListener('input', syncCharacterAmount)
+charAmountRange.addEventListener('input', syncCharacterAmount)
 
 //form to stop the page refreshing(kept getting error at first needed ot put in default to set values before submit plus testing was a pain without it)
 
-form.addEventListener('submit', e => {      //using e for event parameter
+pswdForm.addEventListener('submit', e => {      //using e for event parameter
   e.preventDefault()
-  const characterAmount = characterAmountNumber.value
-  const includeUppercase = includeUppercaseElement.checked
-  const includeNumbers = includeNumbersElement.checked
-  const includeSymbols = includeSymbolsElement.checked
+  const characterAmount = charAmountNumber.value
+  const includeUppercase = includeUppercaseEl.checked       //dom recongised checked mdn:
+  const includeNumbers = includeNumbersEl.checked
+  const includeSymbols = includeSymbolsEl.checked
   const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
   passwordDisplay.innerText = password     //Displays the password when clicked(grabs values)
 })
 //function that combines all of the character code values in a if statment that does the math and prepares for the loop to get random values
 function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
-  let charCodes = LOWERCASE_CHAR_CODES
-  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
-  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
-  
-  const passwordCharacters = []
+  let charCodes = lowercaseChars      //CharCodes takes all strings and numbers and pushes into the pswd characters array (string.fromCharcode(characterCode)) unicode values numbers
+  if (includeUppercase) charCodes = charCodes.concat(uppercaseChars)  //might change
+  if (includeSymbols) charCodes = charCodes.concat(symbolChars)   //might change
+  if (includeNumbers) charCodes = charCodes.concat(numberChars)   //might change
+  console.log(uppercaseChars, symbolChars, numberChars);
+  const pswdCharacters = []
   for (let i = 0; i < characterAmount; i++) {
     const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
-    passwordCharacters.push(String.fromCharCode(characterCode))
+    pswdCharacters.push(String.fromCharCode(characterCode))
   }
-  return passwordCharacters.join('')
+  return pswdCharacters.join('')      //joins all the values in the checkboxes 
 }
 
-function arrayFromLowToHigh(low, high) {        //low and high gauge for the loop sequence returns the newly created array.
-  const array = []
-  for (let i = low; i <= high; i++) {
+function hiLowArray(low, high) {        //low and high gauge for the loop sequence returns the newly created array.
+  const array = []                                //declares new array.
+  for (let i = low; i <= high; i++) {     //iterates and adds index into array.
     array.push(i)
   }
-  return array
+  return array                //returns array
 }
 
-function syncCharacterAmount(e) {
+function syncCharacterAmount(e) {     //syncs the inputs together via event.target trigger = value
   const value = e.target.value
-  characterAmountNumber.value = value
-  characterAmountRange.value = value
+  charAmountNumber.value = value
+  charAmountRange.value = value
 }
